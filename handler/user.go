574c94e25716536c3a7a1099da22865a898c8b62
MRSA <sanggisda@gmail.com>
2023-03-14 14:15:28 +0700

    endpoint avatars,handler avatar

--- handler/user.go
+++ handler/user.go
@@ -123,3 +123,51 @@ func (h *userHandler) CheckEmailAvailability(c *gin.Context){
 		return
 
 }
+
+func (h *userHandler) UploadAvatar(c *gin.Context){
+	// c.SaveUploadedFile(file, )
+	// input dari user
+	// simpan gambarnya di folder "images/"
+	// di service kita panggil repo
+	// JWT (sementara hardcode, seakan2 user yang login ID = 1)
+	// repo ambil data user yang ID = 1
+	// repo update data user simpan lokasi file	
+
+	file, err := c.FormFile("avatar")
+
+	if err != nil {
+		data := gin.H{"is_uploaded":false}
+		response := helper.APIResponse("Failed to upload avatar image", http.StatusBadRequest, "error", data)
+
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+	path := "images/" + file.Filename
+
+	err = c.SaveUploadedFile(file, path)
+	if err != nil {
+		data := gin.H{"is_uploaded":false}
+		response := helper.APIResponse("Failed to upload avatar image", http.StatusBadRequest, "error", data)
+
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	// harusnya dapat dari JWT
+	userID := 1
+
+	_, err = h.userService.SaveAvatar(userID, path)
+
+	if err != nil {
+		data := gin.H{"is_uploaded":false}
+		response := helper.APIResponse("Failed to upload avatar image", http.StatusBadRequest, "error", data)
+
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	data := gin.H{"is_uploaded":true}
+	response := helper.APIResponse("Avatar successfully uploaded", http.StatusOK, "success", data)
+
+	c.JSON(http.StatusOK, response)
+}
