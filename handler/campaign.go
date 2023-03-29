61f03c22178f2eb16bcb96cdd403ba356774b364
MRSA <sanggisda@gmail.com>
2023-03-29 09:55:52 +0700

    upload campaign image endpoint

--- handler/campaign.go
+++ handler/campaign.go
@@ -154,10 +154,17 @@ func (h *campaignHandler) UploadImage(c *gin.Context){
 	err := c.ShouldBind(&input)
 
 	if err != nil {
-		response := helper.APIResponse("Failed to upload campaign image", http.StatusBadRequest, "error", nil)
-		c.JSON(http.StatusBadRequest, response)
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Failed to upload campaign image", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
 		return
 	}
+
+	currentUser := c.MustGet("currentUser").(user.User)
+	input.User = currentUser
+	userID := currentUser.ID
 
 	file, err := c.FormFile("file")
 	if err != nil {
@@ -167,9 +174,6 @@ func (h *campaignHandler) UploadImage(c *gin.Context){
 		c.JSON(http.StatusBadRequest, response)
 		return
 	}
-
-	currentUser := c.MustGet("currentUser").(user.User)
-	userID := currentUser.ID
 
 	path := fmt.Sprintf("images/%d-%s", userID, file.Filename)
 	
