3615ac2dccb8cbcc4e8b48dda86a484343a6f7f9
MRSA <sanggisda@gmail.com>
2023-03-31 16:30:08 +0700

    create handler user trasaction

--- handler/transaction.go
+++ handler/transaction.go
@@ -52,3 +52,17 @@ func (h *transactionHandler) GetCampaignTransactions(c *gin.Context){
 // ambil nilai user dari jwt/middleware
 // service
 // repo => ambil data transactions (preload campaign)
+func (h *transactionHandler) GetUserTransactions(c *gin.Context){
+	currentUser := c.MustGet("currentUser").(user.User)
+	userID := currentUser.ID
+
+	transactions, err := h.service.GetTransactionsByUserID(userID)
+	if err != nil {
+		response := helper.APIResponse("Failed to get user's transaction", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("User's transactions", http.StatusOK, "success", transactions)
+	c.JSON(http.StatusOK, response)
+}
--- transaction/service.go
+++ transaction/service.go
@@ -12,6 +12,7 @@ type service struct {
 
 type Service interface {
 	GetTransactionByCampaignID(input GetCampaignTransactionsInput) ([]Transaction, error)
+	GetTransactionsByUserID(userID int) ([]Transaction, error)
 }
 
 func NewService(repository Repository, campaignRepository campaign.Repository) *service {
@@ -36,5 +37,13 @@ func (s *service) GetTransactionByCampaignID(input GetCampaignTransactionsInput)
 		return transactions, err
 	}
 
+	return transactions, nil
+}
+
+func(s *service) GetTransactionsByUserID(userID int) ([]Transaction, error){
+	transactions, err := s.repository.GetByUserID(userID)
+	if err != nil{
+		return transactions, err
+	}
 	return transactions, nil
 }
