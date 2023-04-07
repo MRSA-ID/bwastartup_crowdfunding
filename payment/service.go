df0e809ba2313b306516fac92f6a0c3043088d87
MRSA <sanggisda@gmail.com>
2023-04-07 07:18:20 +0700

    add payment

---
+++ payment/service.go
@@ -0,0 +1,42 @@
+package payment
+
+import (
+	"bwastartup/user"
+	"strconv"
+
+	"github.com/midtrans/midtrans-go"
+	"github.com/midtrans/midtrans-go/snap"
+)
+
+type service struct {
+}
+
+type Service interface {
+	GetPaymentUrl(transaction Transaction, user user.User) (string, error)
+}
+
+func NewService() *service{
+	return &service{}
+}
+
+func(s *service) GetPaymentUrl(transaction Transaction, user user.User) (string, error){
+
+	midtrans.ServerKey = ""
+	midtrans.Environment = midtrans.Sandbox
+
+	snapReq := &snap.Request{
+		CustomerDetail: &midtrans.CustomerDetails{
+			Email: user.Email,
+			FName: user.Name,
+		},
+		TransactionDetails: midtrans.TransactionDetails{
+			OrderID: strconv.Itoa(transaction.ID),
+			GrossAmt: int64(transaction.Amount),
+		},
+	}
+	snapResp, err := snap.CreateTransaction(snapReq)
+	if err != nil {
+		return "", err
+	}
+	return snapResp.RedirectURL, nil
+}
