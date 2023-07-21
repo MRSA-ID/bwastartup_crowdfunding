8b174ea884f3c98075cca6ff1c56e544830f7ae6
MRSA <sanggisda@gmail.com>
2023-07-21 13:49:59 +0700

    update: entity,repository,service,input -user API

--- user/input.go
+++ user/input.go
@@ -1,5 +1,6 @@
 package user
 
+// special digunakan untuk api
 type RegisterUserInput struct {
 	Name       string `json:"name" binding:"required"`
 	Occupation string `json:"occupation" binding:"required"`
@@ -8,10 +9,27 @@ type RegisterUserInput struct {
 }
 
 type LoginInput struct {
-	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required"`
+	Email    string `json:"email" form:"email" binding:"required,email"`
+	Password string `json:"password" form:"password" binding:"required"`
 }
 
 type CheckEmailInput struct {
 	Email string `json:"email" binding:"required,email"`
+}
+
+// ini digunakan untuk http form
+type FormCreateUserInput struct {
+	Name 				string `form:"name" binding:"required"`
+	Email 			string `form:"email" binding:"required,email"`
+	Occupation 	string `form:"occupation" binding:"required"`
+	Password 		string `form:"password" binding:"required"`
+	Error 			error
+}
+
+type FormUpdateUserInput struct {
+	ID					int
+	Name 				string `form:"name" binding:"required"`
+	Email 			string `form:"email" binding:"required,email"`
+	Occupation 	string `form:"occupation" binding:"required"`
+	Error 			error
 }
